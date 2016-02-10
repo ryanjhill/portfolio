c80b37781a2531ac757f947c92a8090c3af03b35
Ryan Hill <me@ryanjhill.com>
2016-02-10 02:42:14 -0800

    Testing new JS based dynamic footer

---
+++ index.js
@@ -0,0 +1,17 @@
+// Responsive footer
+var bumpIt = function() {
+      $('body').css('margin-bottom', $('#footer').height());
+    },
+    didResize = false;
+
+bumpIt();
+
+$(window).resize(function() {
+  didResize = true;
+});
+setInterval(function() {
+  if(didResize) {
+    didResize = false;
+    bumpIt();
+  }
+}, 250);
